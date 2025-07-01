2d8fc76542064dd355ab41684e775502b2de745d
Strainu <github@strainu.ro>
2025-07-01 16:28:00 +0300

    Tool3: Delay multiple reports while we monitor the user

--- oresreverter/userwarn.py
+++ oresreverter/userwarn.py
@@ -1,11 +1,12 @@
 #!/usr/bin/python3
 # -*- coding: utf-8  -*-
 
-import json
-import pywikibot
+import time
 import requests
 
+import pywikibot
 from pywikibot.tools import is_ip_address
+
 
 class RevertedUser:
 	black_dot = "■"
@@ -16,11 +17,14 @@ class RevertedUser:
 	warn_message = "{{{{subst:au-vandalism{level}{article}|2={{{{subst:evaluare automată}}}} }}}}--~~~~"
 	ip_advice = "{{subst:SharedIPAdvice}}"
 	warn_description = "Avertizare de nivel {level} pentru vandalism la [[{article}]]"
+	report_timestamp = None
 
-	def __init__(self, username: str):
+	def __init__(self, username: str, report_interval_seconds : int = 3600):
 		self.username = username
 		#TODO: dehardcode namespace
 		self.userpage = f"Discuție_Utilizator:{self.username}"
+		self.report_interval_seconds = report_interval_seconds
+		self.report_timestamp = time.time()
 
 	def get_last_warning_level(self) -> int:
 		count = 0
@@ -68,6 +72,14 @@ class RevertedUser:
 		up.put(text, summary=description)
 
 	def report(self):
+		now = time.time()
+		if self.report_timestamp and now - self.report_timestamp < self.report_interval_seconds:
+			pywikibot.info("A report was already made recently. Skipping.")
+			self.report_timestamp = now
+			return
+		else:
+			self.report_timestamp = now
+
 		warn_message = self.block_message.format(user=self.username)
 		description = self.block_description.format(user=self.username)
 		p = pywikibot.Page(pywikibot.Site(), self.block_notify)
